7f9896550a542fcc0184a75fa8854726865fd2c6
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-09 01:22:08 -0400

    Created new solution in order to save time and dynamically create needed lines of code for my Currency Converter Project and SQL Queries. Will be adding queries soon. Formatting JSON response of currencies in Notepad++ right now in order to create an array of strings of sql insert values in order to create the insert queries

---
+++ CreateCurrencyProperties/CreateCurrencyProperties/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateCurrencyProperties
+{
+    /// <summary>
+    /// This program creates all the properties for currencies.
+    /// Output copied in order to use it for the Rate class in my Currency Converter WPF app.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] currency = { "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNH", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STD", "STN", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XDR", "XOF", "XPD", "XPF", "XPT", "YER", "ZAR", "ZMW", "ZWL" };
+
+            foreach (var curr in currency)
+            {
+                // Prints all currency properties
+                // Console.WriteLine("public double "+curr+" { get; set; }");
+
+                // Prints all .Add lines to add data to the data table
+                Console.WriteLine($"dt.Rows.Add(\"{curr}\", val.rates.{curr});");
+
+                // Prints all insert Queries to insert data and values.
+            }
+        }
+    }
+}
